def8b53c572eeeb4ca1360e4e995cf78374d5ad9
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2024-07-14 12:20:31 -0300

    array da lista de usuario criada

---
+++ projeto-cadastro-usuarios/src/app/interfaces/IUser.ts
@@ -0,0 +1,27 @@
+export interface IPerson {
+    user:IUser,
+    musics:IMusics,
+}
+
+interface IUser {
+    name:string,
+    user:string,
+    email:string,
+    password:string,
+    passwordConfirm:string,
+    dateBirth:string,
+    state:number,
+}
+
+interface IMusics {
+    music1:IMusic,
+    music2:IMusic,
+    music3:IMusic,
+}
+
+interface IMusic {
+    title:string,
+    band:string,
+    gender:string
+    favorite:boolean,
+}
